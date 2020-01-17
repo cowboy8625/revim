16a6c5f1b0334061380cbbdaa471a797aefc8afd
cowboy8625 <cowboy8625@yahoo.com>
2020-01-17 10:05:59 -0600

    added part of status bar and welcome message

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-TODO
-====
+TODO List
+=========
 
+- [x] Welcome Message
 - [ ] **Status Bar**
-- [ ] Normal Mode
-- [ ] Command Mode
-- [ ] Insert Mode
-- [ ] Save File
-- [ ] Open Full File
+    - [ ] Modes
+    - [ ] File Type
+    - [ ] File Name
+- [ ] Modes
+    - [ ] Normal Mode
+    - [ ] Command Mode
+    - [ ] Insert Mode
+- [ ] FileIO
+    - [ ] Load File
+    - [ ] Save File
+    - [ ] Open Full File
 - [ ] Scrolling
 - [ ] Syntax HighLighing
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 use crossterm::{
-    cursor::{position, MoveTo},
+    cursor::{MoveTo},
     event::{read, Event, KeyCode},
     queue,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    style::Color::AnsiValue,
+    style::{
+        Color, Print, ResetColor, SetBackgroundColor,
+        SetForegroundColor, Attribute, Styler, StyledContent
+    },
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
     Result,
 };
 
 use std::env;
-use std::io::{BufReader, BufWriter, stdout, Write};
+use std::io::{BufReader, stdout, Write};
 use terminal_size::{terminal_size, Height, Width};
 use std::fs::File;
 use ropey::Rope;
 
+
 /*** data ***/
+
+#[allow(dead_code)]
+enum Mode {
+    COMMAND,
+    NORMAL,
+    INSERT,
+    SEARCH,
+}
+
+struct BfgColor { fg: Color, bg: Color }
 
 struct WinDim{
     top: i16,
@@ -39,6 +54,14 @@ impl WinDim {
     fn pull_left(&mut self) {
         self.left += 3;
     }
+
+    fn push_down(&mut self) {
+        self.left += 3;
+    }
+
+    fn pull_down(&mut self) {
+        self.left -= 3;
+    }
 }
 
 struct Vector {
@@ -52,35 +75,117 @@ impl Vector {
     }
 }
 
+#[allow(dead_code)]
+struct StatusBar {
+    loc: Vector,
+    loc_mode: Vector,
+    loc_ft: Vector,
+    loc_loc: Vector,
+    status_imode: BfgColor,
+    status_nmode: BfgColor,
+    status_cmode: BfgColor,
+    status_loc: BfgColor,
+    status_ft: BfgColor,
+}
+
+#[allow(dead_code)]
+impl StatusBar {
+    fn new(x: i16, y: i16) -> Self {
+        Self {
+            /*** Location ***/
+            loc: Vector::new(x, y),
+
+            /*** Location and color of curser ***/
+            loc_loc: Vector::new(x, y),
+            status_loc: BfgColor{fg: Color::Black, bg: Color::Yellow},
+
+            /*** Location and color of Modes ***/
+            loc_mode: Vector::new(0, y),
+            status_imode: BfgColor{fg: Color::Black, bg: Color::Cyan},
+            status_nmode: BfgColor{fg: Color::Black, bg: Color::Yellow},
+            status_cmode: BfgColor{fg: Color::Black, bg: Color::Magenta},
+
+            /*** Location and color of File Type ***/
+            loc_ft: Vector::new(x, y),
+            status_ft: BfgColor{fg: Color::Cyan, bg: Color::DarkGrey},
+        }
+    }
+    fn render_status_bar(&self, mode: &Mode) {
+        self.status_mode(mode);
+        //self.status_loc(mode);
+        //self.status_file_type(mode);
+    }
+
+    fn status_mode(&self, mode: &Mode) {
+        // Need to figure out the x, and y.
+        // if someone is watching IM SO SORRY FOR THE HORRID CODE. :(
+        let (current, color): (&str, &BfgColor) = match mode {
+            Mode::COMMAND => (" COMMAND ", &self.status_cmode),
+            Mode::NORMAL => (" NORMAL ", &self.status_nmode),
+            Mode::INSERT => (" --insert-- ", &self.status_imode),
+            _ => ("NORMAL", &self.status_nmode),
+        };
+        queue!(
+            stdout(),
+            MoveTo(self.loc_mode.x as u16, self.loc_mode.y as u16),
+            SetForegroundColor(color.fg),
+            SetBackgroundColor(AnsiValue(190)),
+            Print(current.bold()),
+            ResetColor
+        )
+        .unwrap();
+    }
+
+    fn status_loc(&self) {
+        queue!(
+            stdout(),
+            MoveTo(self.loc_loc.x as u16, self.loc_loc.y as u16),
+            SetForegroundColor(color.fg),
+            SetBackgroundColor(AnsiValue(190)),
+            Print(current.bold()),
+            ResetColor
+        )
+        .unwrap();
+    }
+
+    fn status_file_type(&self) {
+    }
+}
+
+#[allow(dead_code)]
 struct Screen {
     c_pos: Vector,
-    c_color: Color,
-    c_blink: bool,
+    s_pos: Vector,
     line_num: bool,
     debug: bool,
+    refeash: bool,
     text: Option<Rope>,
     terminal_size: Vector,
     win_dim: WinDim,
     version: String,
-    bg: Color,
+    status_bar: StatusBar,
+    bg: Color,                 // remove
+    mode: Mode,
 }
 
 impl Screen {
-    fn new(c_pos: Vector, c_color: Color, c_blink: bool, text: Option<Rope>)
+    fn new(c_pos: Vector, text: Option<Rope>)
     -> Self {
 
         let (width, height) = get_terminal_size();
         Self {
             c_pos,
-            c_color,
-            c_blink,
+            s_pos: Vector::new(0, (height - 3) as i16),
             line_num: false,
             debug: false,
+            refeash: false,
             text,
             terminal_size: Vector::new(width as i16, height as i16),
-            win_dim: WinDim::new(0, height as i16, 0, width as i16),
+            win_dim: WinDim::new(0, (height - 3) as i16, 0, width as i16),
             version: String::from("0.0.1"),
+            status_bar: StatusBar::new(width as i16, (height - 2)as i16),
             bg: Color::Black,
+            mode: Mode::NORMAL,
         }
     }
 
@@ -90,6 +195,7 @@ impl Screen {
     }
 
     fn toggle_line_num(&mut self) {
+        self.refeash = true;
         if self.line_num {
             self.line_num = false;
             self.win_dim.push_left();
@@ -101,34 +207,51 @@ impl Screen {
         };
     }
 
-    fn display_welcome(&self) {
-        // x, y for the message start.  Needs Center - text size.
-        let msg = format!("ReVim Version {}", self.version);
-        let x = self.terminal_size.x / 2 - (msg.len() as i16);
-        let y = self.terminal_size.y / 3;
+    fn clear(&self) {
         queue!(
             stdout(),
-            MoveTo(x as u16, y as u16),
-            SetForegroundColor(Color::Cyan),
-            SetBackgroundColor(self.bg),
-            Print(msg),
+            Clear(ClearType::All),
             ResetColor
-        )
-        .unwrap();
+        ).unwrap();
     }
 
-    fn display_numbers(&self, mut start: u16, height: u16, fg: Color, bg: Color) {
-        for y in 0..height {
+    fn display_welcome(&self) {
+        let mut messages = Vec::with_capacity(2);
+        messages.push(
+            format!("ReVim - Rust Edition Vim - Version {}", self.version));
+/*
+        messages.push(
+            "To Quit: :q, To INSERT Text: i, For Help: :help <key or keyword>"
+            .to_string());
+*/
+        messages.push("GitHub: https://www.github.com/cowboy8625/ReVim"
+                      .to_string());
+        for (y, msg) in messages.iter().enumerate() {
+            let x = self.terminal_size.x / 2 - ((msg.len() as i16) / 2);
+            let y = self.terminal_size.y / 3 + y as i16;
             queue!(
                 stdout(),
-                MoveTo(0, y),
-                SetForegroundColor(fg),
-                SetBackgroundColor(bg),
-                Print(start),
+                MoveTo(x as u16, y as u16),
+                SetForegroundColor(Color::Cyan),
+                SetBackgroundColor(self.bg),
+                Print(msg),
                 ResetColor
             )
             .unwrap();
-            start += 1;
+        }
+    }
+
+    fn display_numbers(&self, fg: Color) {
+        for num in self.s_pos.x..self.s_pos.y+1 {
+            queue!(
+                stdout(),
+                MoveTo(0, num as u16),
+                SetForegroundColor(fg),
+                SetBackgroundColor(Color::Black),
+                Print(num + 1),
+                ResetColor
+            )
+            .unwrap();
         }
     }
 
@@ -150,32 +273,33 @@ impl Screen {
         }
     }
 
-    fn display_refresh(&self) -> Result<()> {
-        clear()?;
+    fn display_refresh(&mut self) -> Result<()> {
 
-        if let None = &self.text {
-            self.display_welcome();
-        }
+        if self.refeash { self.clear() }
+        self.status_bar.render_status_bar(&self.mode);
+
+        if let None = &self.text { self.display_welcome(); }
+
         if self.line_num {
-            self.display_numbers(
-                1, self.win_dim.bottom as u16, Color::Green, Color::Black
-                );
+            self.display_numbers(Color::Yellow);
         }
-        if self.debug {
-            self.debug_display();
-        }
+
+        if self.debug { self.debug_display(); }
+
         self.display_text();
+
         queue!(
             stdout(),
             MoveTo(self.c_pos.x as u16, self.c_pos.y as u16),
             ResetColor
         )
         .unwrap();
-        stdout().flush();
+        stdout().flush()?;
         Ok(())
     }
 
-    fn debug_display(&self) {
+    fn debug_display(&mut self) {
+        self.refeash = true;
         queue!(
             stdout(),
             MoveTo(self.win_dim.left as u16, self.win_dim.top as u16),
@@ -238,15 +362,6 @@ fn editor_key_event(e: &mut Screen) -> (bool, bool) {
 
 /*** terminal ***/
 
-fn clear() -> Result<()> {
-    queue!(
-        stdout(),
-        Clear(ClearType::All),
-        ResetColor
-    ).unwrap();
-    stdout().flush();
-    Ok(())
-}
 
 fn get_terminal_size() -> (u16, u16) {
     let size = terminal_size();
@@ -260,12 +375,9 @@ fn get_terminal_size() -> (u16, u16) {
 /*** init ***/
 
 fn main() -> Result<()> {
-    let mut stdout = stdout();
     let mut args = env::args();
 
     enable_raw_mode()?;
-
-    let (width, height) = get_terminal_size();
 
     let mut text = None;
     let _ = args.next();
@@ -275,23 +387,19 @@ fn main() -> Result<()> {
         )?);
     }
 
-    let mut editor = Screen::new(
-        Vector::new(0, 0),
-        Color::White,
-        true,
-        text
-        );
+    let mut editor = Screen::new( Vector::new(0, 0), text);
 
-    clear()?;
+    editor.clear();
     let update = (true, false);
 
     loop {
-        if update.0 { editor.display_refresh(); }
+        if update.0 { editor.display_refresh()?; }
 
         let update = editor_key_event(&mut editor);
 
         if update.1 { break }
     }
+    editor.clear();
     disable_raw_mode()
 }
 
